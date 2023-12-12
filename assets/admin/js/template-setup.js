8d54dea2fb9e4018ffb01cd536b140850a2be32c
jahid-dev <jahidhasancmt7@gmail.com>
2023-12-12 18:25:17 +0600

    tour importer added

--- assets/admin/js/template-setup.js
+++ assets/admin/js/template-setup.js
@@ -37,11 +37,41 @@
         //     }
         // });
 
+        // $.ajax({
+        //     type: 'post',
+        //     url: travelfic_toolkit_script_params.ajax_url,
+        //     data: {
+        //         action: 'travelfic-customizer-settings-import',
+        //         _ajax_nonce: travelfic_toolkit_script_params.travelfic_toolkit_nonce,
+        //     },
+        //     success: function(response1) {
+        //         console.log(response1);
+        //     },
+        //     error: function(error) {
+        //         console.log(error);
+        //     }
+        // });
+
+        // $.ajax({
+        //     type: 'post',
+        //     url: travelfic_toolkit_script_params.ajax_url,
+        //     data: {
+        //         action: 'travelfic-customizer-hotel-import',
+        //         _ajax_nonce: travelfic_toolkit_script_params.travelfic_toolkit_nonce,
+        //     },
+        //     success: function(response1) {
+        //         console.log(response1);
+        //     },
+        //     error: function(error) {
+        //         console.log(error);
+        //     }
+        // });
+
         $.ajax({
             type: 'post',
             url: travelfic_toolkit_script_params.ajax_url,
             data: {
-                action: 'travelfic-customizer-settings-import',
+                action: 'travelfic-customizer-tour-import',
                 _ajax_nonce: travelfic_toolkit_script_params.travelfic_toolkit_nonce,
             },
             success: function(response1) {
