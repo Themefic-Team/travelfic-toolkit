911bb5dec3064a6b14198a9ced74c57def6016e9
jahid-dev <jahidhasancmt7@gmail.com>
2023-12-28 18:10:12 +0600

    socail icon changed

--- inc/customizer/customizer-apply.php
+++ inc/customizer/customizer-apply.php
@@ -186,7 +186,7 @@ $travelfic_design1_topbar_color = get_theme_mod($travelfic_kit_pre.'design_2_top
     .tft-design-2 .tft-top-header .tft-social-share ul li svg path,
     .tft-design-2 .tft-top-header .tft-social-share ul li svg circle,
     .tft-design-2 .tft-top-header .tft-social-share ul li svg ellipse{
-
+        fill: <?php echo esc_attr( $travelfic_design1_topbar_color ).' !important'; ?>;
         stroke: <?php echo esc_attr( $travelfic_design1_topbar_color ).' !important'; ?>;
     }
 </style>
