4d9228221dbeb9b7a15bd4da698c3901cec1ec28
mehedifoysal <mehedifoysal8@gmail.com>
2024-10-03 12:51:11 +0600

    hotel room migrate after import

--- inc/class/class-importer.php
+++ inc/class/class-importer.php
@@ -1067,8 +1067,12 @@ if ( ! class_exists( 'Travelfic_Template_Importer' ) ) {
                     }
                     
                 }
+
+	            $this->travelfic_regenerate_room_meta();
                 wp_die();
             }
+
+
 		}
 
         /**
@@ -1779,6 +1783,55 @@ if ( ! class_exists( 'Travelfic_Template_Importer' ) ) {
                 wp_die();
             }
         }
+
+		function travelfic_regenerate_room_meta() {
+
+			$args  = array(
+				'post_type'      => 'tf_hotel',
+				'post_status'    => 'publish',
+				'posts_per_page' => - 1
+			);
+			$posts = new \WP_Query( $args );
+			if ( $posts->have_posts() ) {
+				while ( $posts->have_posts() ) {
+					$posts->the_post();
+					$post_id = get_the_ID();
+					$meta    = get_post_meta( $post_id, 'tf_hotels_opt', true );
+
+					$rooms = ! empty( $meta['room'] ) ? $meta['room'] : '';
+					if ( ! empty( $rooms ) && gettype( $rooms ) == "string" ) {
+						$tf_hotel_rooms_value = preg_replace_callback( '!s:(\d+):"(.*?)";!', function ( $match ) {
+							return ( $match[1] == strlen( $match[2] ) ) ? $match[0] : 's:' . strlen( $match[2] ) . ':"' . $match[2] . '";';
+						}, $rooms );
+						$rooms                = unserialize( $tf_hotel_rooms_value );
+					}
+
+					$current_user_id = get_current_user_id();
+					foreach ( $rooms as $room ) {
+						$post_data        = array(
+							'post_type'    => 'tf_room',
+							'post_title'   => ! empty( $room['title'] ) ? $room['title'] : 'No Title',
+							'post_status'  => 'publish',
+							'post_author'  => $current_user_id,
+							'post_content' => ! empty( $room['description'] ) ? $room['description'] : '',
+						);
+						$room['tf_hotel'] = $post_id;
+
+						$room_post_id = wp_insert_post( $post_data );
+						update_post_meta( $room_post_id, 'tf_room_opt', $room );
+
+						//insert thumbnail if has 'room_preview_img' key which return url
+						if ( ! empty( $room['room_preview_img'] ) ) {
+							$attachment_id = attachment_url_to_postid( $room['room_preview_img'] );
+							if ( ! empty( $attachment_id ) ) {
+								set_post_thumbnail( $room_post_id, $attachment_id );
+							}
+						}
+					}
+
+				}
+			}
+		}
 	}
 }
 
