a1f84411ad8782a96b1714d83292ef73c844baa3
Mofazzal Hossain <mosharofmd74@gmail.com>
2025-07-09 13:53:28 +0600

    Updated design colors

--- assets/app/js/main.js
+++ assets/app/js/main.js
@@ -3,13 +3,13 @@
 
   $(document).ready(function () {
     // Stiky Menu
-    // $(window).scroll(function () {
-    //   if ($(this).scrollTop() > 0) {
-    //     $(".tft_has_sticky").addClass("tft-navbar-shrink");
-    //   } else {
-    //     $(".tft_has_sticky").removeClass("tft-navbar-shrink");
-    //   }
-    // });
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 0) {
+        $(".tft_has_sticky").addClass("tft-navbar-shrink");
+      } else {
+        $(".tft_has_sticky").removeClass("tft-navbar-shrink");
+      }
+    });
 
     SubMenuHandleEvents();
 
