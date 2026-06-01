6d5359f897f9210e0c047431066fb78b652b5a4c
mehedifoysal <mehedifoysal8@gmail.com>
2026-06-01 14:17:25 +0600

    feat: add body ID and wrap content with page div when Bricks theme is active

--- inc/functions.php
+++ inc/functions.php
@@ -380,3 +380,52 @@ add_action( 'wp_head', function () {
        echo '<style type="text/css" id="travelfic_elementor_background_images">' . $background_images['css_rules'] . '</style>';
    }
 });
+
+/**
+ * Add tft-site-main-body ID in body and wrap the whole page content with <div id="page" class="site"> when Bricks is active.
+ */
+function tft_bricks_theme_body_modifier() {
+    $theme = wp_get_theme();
+    if ( 'bricks' === $theme->template || 'bricks' === $theme->stylesheet ) {
+        // Only run on front-end pages and non-API/non-AJAX requests
+        if ( ! is_admin() && ! wp_doing_ajax() && ! ( defined( 'REST_REQUEST' ) && REST_REQUEST ) ) {
+            ob_start( 'tft_bricks_body_modifier_callback' );
+        }
+    }
+}
+add_action( 'template_redirect', 'tft_bricks_theme_body_modifier' );
+
+function tft_bricks_body_modifier_callback( $buffer ) {
+    if ( empty( $buffer ) || stripos( $buffer, '<body' ) === false ) {
+        return $buffer;
+    }
+
+    // 1. Add id="tft-site-main-body" to <body ...> tag
+    if ( preg_match( '/<body([^>]*)/i', $buffer, $matches ) ) {
+        $body_attributes = $matches[1];
+        
+        if ( strpos( $body_attributes, 'id="tft-site-main-body"' ) === false ) {
+            // Replace any existing id attribute or append it
+            if ( preg_match( '/\bid\s*=\s*["\']([^"\']*)["\']/i', $body_attributes, $id_matches ) ) {
+                $new_body_attributes = preg_replace( '/\bid\s*=\s*["\']([^"\']*)["\']/i', 'id="tft-site-main-body"', $body_attributes );
+            } else {
+                $new_body_attributes = $body_attributes . ' id="tft-site-main-body"';
+            }
+            
+            $buffer = str_replace( $matches[0], '<body' . $new_body_attributes, $buffer );
+        }
+    }
+
+    // 2. Wrap page content with <div id="page" class="site">...</div>
+    if ( preg_match( '/<body[^>]*>/i', $buffer, $matches ) ) {
+        $body_tag = $matches[0];
+        
+        if ( strpos( $buffer, '<div id="page" class="site">' ) === false ) {
+            $buffer = str_replace( $body_tag, $body_tag . '<div id="page" class="site">', $buffer );
+            $buffer = preg_replace( '/<\/body>/i', '</div></body>', $buffer );
+        }
+    }
+
+    return $buffer;
+}
+
